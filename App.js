d3fd23160b1db8ffebbdb5b69f957f392e100925
Shravankumar0219 <shravankumar62379@gmail.com>
2024-07-08 17:53:12 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.root}>
+      <Text style={styles.text}>SharpTech New Mobile Application Project</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24, // Adjust the font size as needed
+    textAlign: 'center',
+    color: 'blue'
+  },
+})
